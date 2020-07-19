7c7a5f5e5ad93830ee078b1e5d579744e91ff0f9
Nicole Graiff <nicole.graiff@gmail.com>
2020-07-18 22:28:57 -0400

    Added /api/notes GET route. Added .gitignore.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -12,6 +19,12 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
     console.log("This message is inside the get '/notes' function");
   });
+
+app.get("/api/notes", function(req, res) {
+    let notesJSON = fs.readFileSync("db/db.json");
+    console.log(`notesJSON = ${notesJSON}`);
+    res.json(JSON.parse(notesJSON));
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
