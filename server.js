c84bc1c052ead6fdaeaa52ee1de0815402475b74
Nicole Graiff <nicole.graiff@gmail.com>
2020-07-24 13:21:07 -0400

    Added delete function part 1 - delete item from JSON file.

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
-
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var jquery = require("jquery");
+const { Console } = require("console");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
 // ==============================================================================
-
 // Tells node that we are creating an "express" server
 var app = express();
 
@@ -22,9 +22,6 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-// Master ID for notes
-let masterID = 0;
 
 // ================================================================================
 // ROUTES
@@ -41,13 +38,11 @@ app.get("/notes", function(req, res){
 //API
 app.get("/api/notes", function(req, res){
     let notesJSON = fs.readFileSync("db/db.json") ||[];
-    console.log(`notesJSON = ${notesJSON}`);
     if(notesJSON){
-      res.json(JSON.parse(notesJSON));
+      return res.json(JSON.parse(notesJSON));
     } else {
-      res.send("There are no stored notes yet.");
+      return res.send("There are no stored notes yet.");
     }
-    
 });
 
 app.post("/api/notes", function(req, res){
@@ -66,6 +61,27 @@ app.post("/api/notes", function(req, res){
 
     return res.send(console.log("Success!"));
 });
+
+  app.delete("/api/notes/:id", function(req, res){
+    let selectedID = req.params.id;
+      console.log(`The selectedID = ${selectedID}`);
+    let notesArray = JSON.parse(fs.readFileSync("db/db.json")) ||[];
+      console.log(`notesArray = ${JSON.stringify(notesArray)}`);
+    for(let i = 0; i < notesArray.length; i++){
+      console.log("We made it inside the for loop");
+      if(parseInt(notesArray[i].id) == selectedID){
+        console.log(`The index is ${i}, the selectedID = ${selectedID}, and the notesArray[i].id = ${notesArray[i].id}`);
+        console.log
+        notesArray.splice(i, 1);
+        fs.writeFileSync("db/db.json", JSON.stringify(notesArray));
+        return console.log(`The notesArray after splice = ${JSON.stringify(notesArray)}`);
+      }else{
+        console.log(`The index is ${i}, and we do not have a match`);
+        console.log(`The selectedID = ${selectedID} and he notesArray[i].id = ${notesArray[i].id}`);
+      }
+    }
+    return res.send(console.log("The db.json file should have been updated!"));
+  });
 
 // =============================================================================
 // LISTENER
