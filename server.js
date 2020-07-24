f7603c8789fbb0e436b244e67209bf3d58dd2e35
Nicole Graiff <nicole.graiff@gmail.com>
2020-07-24 14:02:35 -0400

    Fully functioning delete function.

--- server.js
+++ server.js
@@ -5,8 +5,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var jquery = require("jquery");
-const { Console } = require("console");
+
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -39,48 +38,33 @@ app.get("/notes", function(req, res){
 app.get("/api/notes", function(req, res){
     let notesJSON = fs.readFileSync("db/db.json") ||[];
     if(notesJSON){
-      return res.json(JSON.parse(notesJSON));
-    } else {
-      return res.send("There are no stored notes yet.");
+      res.json(JSON.parse(notesJSON));
     }
 });
 
 app.post("/api/notes", function(req, res){
     let retrieveID = JSON.parse(fs.readFileSync("db/id.json")) || 0;
-    console.log(`The retrieved ID = ${retrieveID}`);
     let updatedNotesArray = JSON.parse(fs.readFileSync("db/db.json")) || [];
     let note = req.body;
-    console.log(`RetrieveID = ${retrieveID.masterID}`)
     note.id = retrieveID.masterID + 1;
-    console.log(`note.id = ${note.id}`);
     masterID = {"masterID": note.id};
-    console.log(`The masterID = ${masterID}`);
     updatedNotesArray.push(note);
     fs.writeFileSync("db/db.json", JSON.stringify(updatedNotesArray));
     fs.writeFileSync("db/id.json", JSON.stringify(masterID));
-
-    return res.send(console.log("Success!"));
+    
+    res.send(console.log("Success! Your new note has been saved to the db.json file and the master id has been updated."));
 });
 
   app.delete("/api/notes/:id", function(req, res){
     let selectedID = req.params.id;
-      console.log(`The selectedID = ${selectedID}`);
     let notesArray = JSON.parse(fs.readFileSync("db/db.json")) ||[];
-      console.log(`notesArray = ${JSON.stringify(notesArray)}`);
     for(let i = 0; i < notesArray.length; i++){
-      console.log("We made it inside the for loop");
       if(parseInt(notesArray[i].id) == selectedID){
-        console.log(`The index is ${i}, the selectedID = ${selectedID}, and the notesArray[i].id = ${notesArray[i].id}`);
-        console.log
         notesArray.splice(i, 1);
         fs.writeFileSync("db/db.json", JSON.stringify(notesArray));
-        return console.log(`The notesArray after splice = ${JSON.stringify(notesArray)}`);
-      }else{
-        console.log(`The index is ${i}, and we do not have a match`);
-        console.log(`The selectedID = ${selectedID} and he notesArray[i].id = ${notesArray[i].id}`);
       }
     }
-    return res.send(console.log("The db.json file should have been updated!"));
+    res.send(console.log("Successful deletion! The db.json file has been updated."));
   });
 
 // =============================================================================
