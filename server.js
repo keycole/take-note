33305891cb6923f09c582a8064bb030c4de2b039
Nicole Graiff <nicole.graiff@gmail.com>
2020-07-24 14:21:44 -0400

    Cleaned up server.js and added notes.

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-// ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
-
-// ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-// Tells node that we are creating an "express" server
 var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-// ================================================================================
 // ROUTES
-// ================================================================================
-//HTML
+// >> HTML ROUTES
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
@@ -34,7 +21,7 @@ app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-//API
+// >> API ROUTES
 app.get("/api/notes", function(req, res){
     let notesJSON = fs.readFileSync("db/db.json") ||[];
     if(notesJSON){
@@ -43,35 +30,45 @@ app.get("/api/notes", function(req, res){
 });
 
 app.post("/api/notes", function(req, res){
-    let retrieveID = JSON.parse(fs.readFileSync("db/id.json")) || 0;
+  //Retrieves the last id from the id.json file
+    let retrieveMasterID = JSON.parse(fs.readFileSync("db/id.json")) || 0;
+  //Retrieves the stored notes array from the db.json file
     let updatedNotesArray = JSON.parse(fs.readFileSync("db/db.json")) || [];
+  //Gets the new note information from the request body on the /notes page
     let note = req.body;
-    note.id = retrieveID.masterID + 1;
+  //Uses the retrieved master id to create a unique id for the new note
+    note.id = retrieveMasterID.masterID + 1;
+  //Updates the masterID object with the new note's ID
     masterID = {"masterID": note.id};
+  //Add the new note to the saved notes array
     updatedNotesArray.push(note);
+  //Save the new notes array to the db.json file (overwrites old file)
     fs.writeFileSync("db/db.json", JSON.stringify(updatedNotesArray));
+  //Save the updated masterID to the id.json file (overwrites old file)
     fs.writeFileSync("db/id.json", JSON.stringify(masterID));
-    
+  //console.log success
     res.send(console.log("Success! Your new note has been saved to the db.json file and the master id has been updated."));
 });
 
   app.delete("/api/notes/:id", function(req, res){
+    //Retrieve the selected note ID from the request parameters placeholder ":/id"
     let selectedID = req.params.id;
+    //Retrieve the saved notes array from the db.json file
     let notesArray = JSON.parse(fs.readFileSync("db/db.json")) ||[];
+    //Loop through the notes array to find the note whose ID matches the selected ID
     for(let i = 0; i < notesArray.length; i++){
       if(parseInt(notesArray[i].id) == selectedID){
+        //splice the selected note out of the saved notes array
         notesArray.splice(i, 1);
+        //save the new notes array (minus the deleted note) to the db.json file (overwrites old file)
         fs.writeFileSync("db/db.json", JSON.stringify(notesArray));
       }
     }
+    //console.log success
     res.send(console.log("Successful deletion! The db.json file has been updated."));
   });
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
+// LISTENER to start the server
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
